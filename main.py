e83d883768ff18f6b1148b4e190b376ad236725f
awekrx <d.ipatov.v@gmail.com>
2023-03-01 13:03:00 +0200

    update

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from modules.prompt import Prompt
 import configparser
 
-config = configparser.ConfigParser()  
-config.read("config.ini") 
+config = configparser.ConfigParser()
+config.read("config.ini")
 
 auth = {
     "email": config["ChatGPT"]["email"],
     "password": config["ChatGPT"]["password"],
-    "session_token": config["ChatGPT"]["session_token"]
+    "session_token": config["ChatGPT"]["session_token"],
 }
 
 prompter = Prompt(auth)
